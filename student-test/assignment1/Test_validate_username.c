2ab1e28bccfef14e8b94f43707e9478eecc6bf02
VasuPadsumbia <vasu.padsumbia@gmail.com>
2025-06-05 15:29:39 +0200

    autotest checkout

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,9 +18,10 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
+    char *username = malloc_username_from_conf_file();
     TEST_ASSERT_EQUAL_STRING_MESSAGE(
         my_username(),
-        malloc_username_from_conf_file(),
+        username,
         "Username from conf file does not match hard coded username in autotest-validate.c"
     );
     //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
